b5d9b5df442032a177e253c2d3aafcde8f3b9228
Sergei Kostiaev <88935925+SergeiKostiaev@users.noreply.github.com>
2025-05-06 16:42:13 +0400

    delete config.py

--- config.py
+++ config.py
@@ -1,4 +1,1 @@
-TOKEN = '7981371119:AAEEqhnc-Xb4nk3Rn8zGN3TPTj__fo-5pGM'
-AGREEMENT_URL = 'https://drive.google.com/your-agreement'
-PRIVACY_URL = 'https://drive.google.com/your-privacy'
-ADMIN_ID = '414951154'
+
